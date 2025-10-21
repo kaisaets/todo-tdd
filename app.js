3f451c0bd52651c590a375c4338bb878e1d46f97
kaisa <kaisaets@gmail.com>
2025-10-21 15:51:21 +0300

    error handlig for unit and integration tests

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ mongodb.connect()
 
 app.use(express.json())
 app.use("/todos", todoRoutes)
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message })
+})
 
 app.get('/', (req, res) => {
     res.send('express test')
